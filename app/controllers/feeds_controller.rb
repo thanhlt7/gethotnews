665fb982239dc509d11ff9f129a084844d395e60
thanhlt7 <thanhle289@gmail.com>
2016-04-09 01:24:45 +0700

    Add dropdown category

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -69,6 +69,6 @@ class FeedsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
-      params.require(:feed).permit(:name, :url, :description)
+      params.require(:feed).permit(:name, :url, :description, :category_id)
     end
 end
