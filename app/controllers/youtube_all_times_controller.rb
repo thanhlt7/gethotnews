bdba1482ea8fc5ffde9993e23e11f4470d698ac5
thanhlt7 <thanhle289@gmail.com>
2016-04-17 00:45:22 +0700

    Change order field

--- app/controllers/youtube_all_times_controller.rb
+++ app/controllers/youtube_all_times_controller.rb
@@ -1,5 +1,5 @@
 class YoutubeAllTimesController < ApplicationController
 	def index
-    	@entries = YoutubeAllTime.all.paginate(page: params[:page], per_page: 6).order('id asc')
+    	@entries = YoutubeAllTime.all.paginate(page: params[:page], per_page: 6).order('viewcount asc')
     end
 end
