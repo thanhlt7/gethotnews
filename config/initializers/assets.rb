35a866ce6e083e0275e9d1dda6f83632c83eba16
thanhlt7 <thanhle289@gmail.com>
2016-04-27 00:40:50 +0700

    Revert assets.rb file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( style.css )
-Rails.application.config.assets.precompile += %w( owl.carousel.css )
-Rails.application.config.assets.precompile += %w( popuo-box.css )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
