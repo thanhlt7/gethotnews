2a4c77a947e1e6c1d96a1c2320e3a016c0527603
thanhlt7 <thanhle289@gmail.com>
2016-05-01 18:04:01 +0700

    Still a bug

--- app/controllers/youtube_all_times_controller.rb
+++ app/controllers/youtube_all_times_controller.rb
@@ -1,5 +1,5 @@
 class YoutubeAllTimesController < ApplicationController
 	def index
-    	@entries = YoutubeAllTime.all.limit(20).paginate(page: params[:page], per_page: 10).order('viewcount desc')
+    	@entries = YoutubeAllTime.limit(20).paginate(page: params[:page], per_page: 10).order('viewcount desc')
     end
 end
